d6d3f64ff3e08ae040826a6d365dd62956960e39
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2023-10-17 14:51:44 +0000

    changes the code

--- Two_pointers/Valid_Palindrome.py
+++ Two_pointers/Valid_Palindrome.py
@@ -8,33 +8,36 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 '''
 #Implement main  function 
 
-def isPalindrome(self, s:str) -> bool:
-    l, r = 0, len(s) - 1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
     
-    while l < r:
-        while l < r and not self.alphanum(s[i]):
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+        
             l += 1
-            
-        while l < r and not self.alphanum(s[r]):
             r -= 1
-            
-            
-        if s[l].lower() != s[r].lower():
-            return False
         
-        l += 1
-        r -= 1
-        
-    return True
+        return True
     
-        
-        
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord('z')
+            or ord("0") <= ord(c) <= ord('9')
+        )
 
+# Create an instance of the Solution class
+solution = Solution()
 
-#Helper function alpha-numeric function
-def alphanum(self,c):
-    return (
-        ord("A") <= ord(c) <= ord("Z")
-        or ord("a") <=ord(c) <= ord('z')
-        or ord("0") <=ord(c) <= ord('9')
-    )
+# Test the isPalindrome function with an example string
+s = "aba"
+result = solution.isPalindrome(s)
+print(result)
--- Two_pointers/buy_sell_1.py
+++ Two_pointers/buy_sell_1.py
@@ -1,1 +1,46 @@
-print("Hello Masum")
+
+#Leet code 121:
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        #Goal to return the maximum profit if not then return 0
+        #if I use the brute force algorithm then the complexity with O(n2) which will not be 
+        #efficient then apply sliding window will be more efficient(better complexity)
+        
+
+        #Intialize two pointers , max_profit variable(which will be return)
+
+        l = 0
+        r = 1
+
+        max_profit = 0
+
+        #Iterate through the every price of prices of the day
+        while r < len(prices):
+
+            #Find the present pattern
+            current_profit = prices[r] - prices[l]
+
+            # Checking the conditon and calculate the result
+            if prices[l] < prices[r]:
+                max_profit = max(current_profit, max_profit)
+
+            else:  #update the left pointer
+                l = r 
+
+            #Then increment by 1 of the right pointer
+            r += 1
+
+        return max_profit
+
+nums = [7,1,5,3,6,4]
+solution = Solution()
+result = solution.maxProfit(nums)
+print(result)
+
+
+
+
+
