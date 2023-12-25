cc9201289d37334fc3fcf7e3c2b45a03a2dd60ca
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2023-12-25 16:52:36 +0000

    Have changed the code

--- Dynamic_Programing/howSum.py
+++ Dynamic_Programing/howSum.py
@@ -1,5 +1,8 @@
 
-def howSum(targerSum, numbers):
+def howSum(targerSum, numbers,memo={}):
+
+    if targerSum in memo:
+        return memo[targerSum]
     #Base case
     if targerSum == 0:
         return []
@@ -10,15 +13,20 @@ def howSum(targerSum, numbers):
     #Recursive case
     for num in numbers:
         remainder = targerSum -num
-        remainderResult = howSum(remainder, numbers)
+        remainderResult = howSum(remainder, numbers, memo)
 
         if remainderResult is not  None:
             #Copy the whole remainderResult like this (...remainderResult)
-            return [*remainderResult, num]
+            #store in the memo
+            memo[targerSum] = [*remainderResult, num]
+            return memo[targerSum]
+
         
-    
+    memo[targerSum] = None
     return None
-    
+
+#(for dp)->Time:O(n*m^2), Space:O(m^2); m = targerSum, n= array length
+
 target = 7
 nums = [2, 3]
 result = howSum(target, nums)
