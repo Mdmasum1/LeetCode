ece6a24ecf16a9de81f084da96ccc2cc4eee96f3
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2023-12-25 16:15:39 +0000

    have added the code

--- Dynamic_Programing/howSum.py
+++ Dynamic_Programing/howSum.py
@@ -1,3 +1,27 @@
 
 def howSum(targerSum, numbers):
+    #Base case
+    if targerSum == 0:
+        return []
     
+    if targerSum < 0:
+        return None
+    
+    #Recursive case
+    for num in numbers:
+        remainder = targerSum -num
+        remainderResult = howSum(remainder, numbers)
+
+        if remainderResult is not  None:
+            #Copy the whole remainderResult like this (...remainderResult)
+            return [*remainderResult, num]
+        
+    
+    return None
+    
+target = 7
+nums = [2, 3]
+result = howSum(target, nums)
+print(f"Numbers that sum up to {target}: {result}")
+
+#Output: Numbers that sum up to 7: [3,2, 2]
