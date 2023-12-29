df228bb98863017d4fea9652d4353df5479acb29
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2023-12-29 04:58:33 +0000

    Have changed the code

--- Heap/621_leastInterval.py
+++ Heap/621_leastInterval.py
@@ -1,5 +1,30 @@
 from collections import Counter
 import heapq
+'''
+---Leet code 621:Task Scheduler:
+  Given, a characters array tasks, representing the tasks a 
+  CPU needs to do, where each letter represents a different
+  task. Tasks could be done in any order.Each task is done one unit 
+  of time. For each unit of time, the CPU could complete either 
+  one task or just be idle.
+
+   However, there is a non-negative integer n that represents
+   the cooldown perid between two same tasks(the same letter in the 
+   array), that is that there must be at least n units of time between
+   any two same tasks.
+
+   Return the least number of units of times that the CPU will take
+   to finish all the given tasks.
+
+   Example 1:
+
+   Input: tasks = ["A", "A", "A", "B", "B", "B"], n = 2
+   Explanation: A-> B-> idle -> A -> B -> idle -> A -> B
+   There is at least 2 units of time between any two same tasks
+   
+
+'''
+
 
 def leastInterval(tasks, n):
     
