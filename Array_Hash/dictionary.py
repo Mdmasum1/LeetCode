b0769aafc99325e8c2d908ef410a654b62f197ff
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-01-08 22:08:30 +0000

    have added the file

---
+++ Array_Hash/dictionary.py
@@ -0,0 +1,7 @@
+
+#Use dictionary to store information about a person you know.
+#Store their first name, last name, age, city in which they live 
+#.YOu should have keys such as first_name, last_name, age and city.
+#Print each piece of information stored in your dictionary
+
+
