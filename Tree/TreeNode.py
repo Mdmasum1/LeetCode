50e6b38ab4dda9352e0098ac9ae980b5215c2b3b
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2026-06-27 12:05:28 +0000

    implemneted

---
+++ Tree/TreeNode.py
@@ -0,0 +1,10 @@
+
+#Every tree has common class TreeNode
+class TreeNode:
+
+    #Default constructor
+    def __init__(self, val=0, left=None, right=None):
+        self.val= val
+        self.left= left
+        self.right = right 
+        
