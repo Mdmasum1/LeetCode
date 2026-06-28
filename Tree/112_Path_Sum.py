3aca511314c089679a8c63cde6b91957c8ee3311
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2026-06-28 14:45:08 +0000

    have implemented recursive method

--- Tree/112_Path_Sum.py
+++ Tree/112_Path_Sum.py
@@ -28,31 +28,46 @@ class TreeNode:
 class Solution:
 
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        #Base case
+        # Bad base case
         if root is None:
             return False
 
-        #Initialize stack with root node and its value root.val  
-        stack = [(root, root.val)]
+        #Good base case: if curSum is equalt to target and it is leaf node
+        if root.left is None and root.right is None and targetSum == root.val:
+            return True
+        
+        #either left or right matching the current sum then return true
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
 
-        while stack:
-            node, curSum = stack.pop()
+        #T:O(n); s:O(N); time compelxity n becuse of every node function call happen and space complexity n because using stack take n space 
 
-            if node.left is None and node.right is None and curSum == targetSum:
-                return True
+    # #------------Iterative Method--------------
+    # def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+    #     #Base case
+    #     if root is None:
+    #         return False
 
-            #From this point looking for root's child
-            if node.left:
-                stack.append((node.left, node.left.val + curSum))
-            if node.right:
-                stack.append((node.right, node.right.val + curSum))
+    #     #Initialize stack with root node and its value root.val  
+    #     stack = [(root, root.val)]
 
-        return False
+    #     while stack:
+    #         node, curSum = stack.pop()
+
+    #         if node.left is None and node.right is None and curSum == targetSum:
+    #             return True
+
+    #         #From this point looking for root's child
+    #         if node.left:
+    #             stack.append((node.left, node.left.val + curSum))
+    #         if node.right:
+    #             stack.append((node.right, node.right.val + curSum))
+
+    #     return False
 
 #T:O(n); S:O(N) ; complexity for the wrost case scenario.
 
-# Quick manual test
+# Quick manual test  ; Create root object from the TreeNode class
 root = TreeNode(5)
 root.left = TreeNode(4)
 root.right = TreeNode(8)
@@ -63,7 +78,7 @@ root.left.left.right = TreeNode(2)
 sol = Solution()
 print(sol.hasPathSum(root, 22))  # Expected: True
 print(sol.hasPathSum(root, 99))  # Expected: False
-print(sol.hasPathSum(None, 99))  # Expected: False
+print(sol.hasPathSum(None, 22))  # Expected: False
 
 
 
